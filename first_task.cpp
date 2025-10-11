9aafeee229d1feabd490b40137de5988f26dd0d3
PSKosareva <PSkosareva@stud.kantiana.ru>
2025-10-11 12:59:25 +0200

    second lab

---
+++ first_task.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <bitset>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	short A;
+	int i;
+
+	std::cout << "Введите число А: " << std::endl;
+	std::cin >> A;
+
+	std::cout << "Введите номер бита (0-7): " << std::endl;
+	std::cin >> i;
+
+	//проверка
+
+	if (i < 0 || i > 7) {
+		std::cout << "Ошибка! Номер бита должен быть от 0 до 7." << std::endl;
+		return 1;
+	}
+
+	std::bitset<8> bitsA(A);
+	std::cout << A << " = " << bitsA << std::endl;
+
+	int bitValue = bitsA[i]; 
+	std::cout << "Значение " << i << "-го бита — " << bitValue << std::endl;
+
+	if (bitValue != 0) {
+		bitsA ^= std::bitset<8>(1 << i); 
+		A = static_cast<int>(bitsA.to_ulong()); 
+
+		std::cout << "Результат после инверсии " << i << "-го бита: "
+			<< A << " = " << bitsA << std::endl;
+	}
+	else { 
+		int num1, num2;
+		std::cout << "Введите два числа: " << std::endl;
+		std::cin >> num1 >> num2;
+		int abs1, abs2;
+		if (num1 <= 0) {
+			abs1 = -num1;
+		}
+		else {
+			abs1 = num1;
+		}
+
+		if (num2 <= 0) {
+			abs2 = -num2;
+		}
+		else {
+			abs2 = num2;
+		}
+
+		int maxAbs;
+
+		if (abs1 > abs2) {
+			maxAbs = num1;
+		}
+		else if (abs2 > abs1) {
+			maxAbs = num2;
+		}
+		else {
+			maxAbs = num1;
+		}
+
+		std::cout << "Результат: " << maxAbs << maxAbs << std::endl;
+	}
+	return 0;
+}
